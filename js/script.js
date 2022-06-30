044bfad0dd450f3717cddd803d2e8ef1fa7add49
Sapik <Sapik_19@mail.ru>
2022-06-30 18:16:41 +0300

    Adaptive navigation menu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', function() {
-    
+    //Бургер меню
+    let burger = document.querySelector('#menu_checkbox'),
+    burgerHidden = document.querySelector('.header__wrapper-burger');
+
+    burger.addEventListener('click', (e) =>{
+        
+        if (burgerHidden.classList.contains('header__wrapper-burger')) {
+            burgerHidden.classList.toggle('header__wrapper-burger_active');
+        } 
+        console.dir(burgerHidden);
+    });
 });
